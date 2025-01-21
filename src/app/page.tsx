34403b051a52882216761741df979238efcd6ad4
samiyamarium <160397803+samiyamarium@users.noreply.github.com>
2025-01-21 23:50:00 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { client } from "../app/sanity/client";
 const POSTS_QUERY = `*[
   _type == "post"
   && defined(slug.current)
-]|order(publishedAt desc)[0...23]{_id, title, slug, publishedAt}`;
+]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
 
 const options = { next: { revalidate: 30 } };
 
